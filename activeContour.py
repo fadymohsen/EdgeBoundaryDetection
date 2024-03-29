8cf7664909ad4cad3d91ef8c9ee39f1f7a743ab7
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2024-03-29 17:58:14 +0200

    Canny Edges Step

--- activeContour.py
+++ activeContour.py
@@ -10,21 +10,23 @@ class ActiveContour:
         self.image_path = image_path
         self.gray_image, self.edge_image = self.load_and_process_image()
 
+
+
     def load_and_process_image(self):
-        """Loads the image, converts it to grayscale, and applies Canny edge detection."""
-        gray_image = cv2.imread(self.image_path, cv2.IMREAD_GRAYSCALE)
-        if gray_image is None:
+        if self is None:
             print(f"Failed to load image from {self.image_path}")
             return None, None
         
+        gray_image = cv2.imread(self.image_path, cv2.IMREAD_GRAYSCALE)
+        print("\nSUCCESS - Image is converted to GreyScale")
         edge_image = cv2.Canny(gray_image, 30, 150)
-        print("\n\nImage loaded and processed\n\n")
+        print("SUCCESS - Image is converted to Canny Edges\n\n")
         return gray_image, edge_image
 
+
+
     def get_gray_image_data(self):
-        """Returns the grayscale image data."""
         return self.gray_image
 
     def get_edge_image_data(self):
-        """Returns the Canny edge processed image data."""
         return self.edge_image
