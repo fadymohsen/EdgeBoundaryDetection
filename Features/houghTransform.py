6ab307a330765049d8895db526b6dc3bce2cff89
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2024-03-31 23:50:17 +0200

    Update Files

--- Features/houghTransform.py
+++ Features/houghTransform.py
@@ -3,11 +3,17 @@ import cv2
 import pyqtgraph as pg
 from pyqtgraph import ImageItem
 
+
+
+
+
 class houghTransformShapeDetection:
     def __init__(self, main_tab_widget):
         self.main_tab_widget = main_tab_widget
         self.ui = self.main_tab_widget
         self.ui.comboBox_houghDetectionMethod.activated.connect(self.detectShape)
+
+
 
     def detectShape(self):
         if self.main_tab_widget.selected_image_path:
@@ -25,26 +31,18 @@ class houghTransformShapeDetection:
         else:
             self.detectCircles(edgeDetectedImage, 10,15,radius=[50,5])
 
+
+
     def detectCircles(self, image, threshold,region,radius = None):
-        # circles = cv2.HoughCircles(image,cv2.HOUGH_GRADIENT,1,20,
-        #             param1=200,param2=30,minRadius=20,maxRadius=200)
-        
         (M,N) = image.shape
         if radius == None:
             R_max = np.max((M,N))
             R_min = 3
         else:
             [R_max,R_min] = radius
-
         R = R_max - R_min
-        #Initializing accumulator array.
-        #Accumulator array is a 3 dimensional array with the dimensions representing
-        #the radius, X coordinate and Y coordinate resectively.
-        #Also appending a padding of 2 times R_max to overcome the problems of overflow
         A = np.zeros((R_max,M+2*R_max,N+2*R_max))
         B = np.zeros((R_max,M+2*R_max,N+2*R_max))
-
-        #Precomputing all angles to increase the speed of the algorithm
         theta = np.arange(0,360)*np.pi/180
         edges = np.argwhere(image[:,:])                                               #Extracting all edge coordinates
         for val in range(R):
@@ -58,8 +56,6 @@ class houghTransformShapeDetection:
                 bprint[m+x,n+y] = 1
             constant = np.argwhere(bprint).shape[0]
             for x,y in edges:                                                       #For each edge coordinates
-                #Centering the blueprint circle over the edges
-                #and updating the accumulator array
                 X = [x-m+R_max,x+m+R_max]                                           #Computing the extreme X values
                 Y= [y-n+R_max,y+n+R_max]                                            #Computing the extreme Y values
                 A[r,X[0]:X[1],Y[0]:Y[1]] += bprint
@@ -73,15 +69,11 @@ class houghTransformShapeDetection:
                 continue
             B[r+(p-region),x+(a-region),y+(b-region)] = 1
         circles = B[:,R_max:-R_max,R_max:-R_max]
-        
         image_with_circles = np.copy(self.imageArray)
         image_with_circles_color = cv2.cvtColor(image_with_circles, cv2.COLOR_GRAY2BGR)
-
         # Draw the circles as borders
         for r, x, y in np.argwhere(circles):
             cv2.circle(image_with_circles_color, (y, x), r, (0, 255, 0), 3)
-        
-        
         # Clear existing items in the appropriate view box
         self.ui.graphics_afterHoughDetection.clear()
         view_box = self.ui.graphics_afterHoughDetection.addViewBox()
@@ -91,8 +83,8 @@ class houghTransformShapeDetection:
         view_box.addItem(img_item)
     
 
+
     def detectLines(self, image):
-    
         rho = 1
         theta = np.pi / 180
         threshold = 500
@@ -102,13 +94,16 @@ class houghTransformShapeDetection:
         return lines, detected_image
 
 
+
     def displayLines(self, image, lines):
         for line in lines:
             x0, y0, x1, y1 = line
             cv2.line(image, (int(x0), int(y0)), (int(x1), int(y1)), (0, 0, 255), 2)
-
         # Display the image with detected lines
         self.displayImage(image)
+
+
+
 
     def displayImage(self, image):
         # Assuming you have QGraphicsView or similar for displaying images
@@ -127,13 +122,9 @@ class houghTransformShapeDetection:
         diag_len = np.ceil(np.sqrt(height * height + width * width))
         rho_bins = np.int32(diag_len / rho)
         theta_bins = np.int32(np.pi / theta)
-
         accumulator = np.zeros((rho_bins, theta_bins), dtype=np.uint8)
-
         y, x = np.nonzero(img)
-
         num_edge_points = len(x)
-
         for i in range(num_edge_points):
             for theta_index in range(theta_bins):
 
@@ -168,6 +159,9 @@ class houghTransformShapeDetection:
 
 
 
+
+
+
     def cannyEdgeDetection(self, image):
         blurred_img = cv2.GaussianBlur(image, (5, 5), 4)
 
@@ -178,19 +172,15 @@ class houghTransformShapeDetection:
         sobelFilterHorizontal = np.array([[-1, -2, -1],
                                             [0, 0, 0],
                                             [1, 2, 1]])
-
         Gradient_x = cv2.filter2D(blurred_img, -1, sobelFilterVertical) 
         Gradient_y = cv2.filter2D(blurred_img, -1, sobelFilterHorizontal) 
-
         Gradient = np.hypot(Gradient_x, Gradient_y)
         Gradient = Gradient / Gradient.max() * 255
         angle = np.arctan2(Gradient_y, Gradient_x)
-
         M, N = Gradient.shape
         Z = np.zeros((M, N), dtype=np.int32)
         angle = angle * 180 / np.pi
         angle[angle < 0] += 180
-
         for i in range(1, M - 1):
             for j in range(1, N - 1):
                 q = 255
@@ -213,7 +203,6 @@ class houghTransformShapeDetection:
                 else:
                     Z[i, j] = 0
         thinEdgesImage = Z
-
         lowThresholdRatio = 0.05
         highThresholdRatio = 0.09
         highThreshold = thinEdgesImage.max() * highThresholdRatio
@@ -227,7 +216,6 @@ class houghTransformShapeDetection:
         weak_i, weak_j = np.where((thinEdgesImage <= highThreshold) & (thinEdgesImage >= lowThreshold))
         thresholdedImage[strong_i, strong_j] = strong
         thresholdedImage[weak_i, weak_j] = weak
-
         for i in range(1, M - 1):
             for j in range(1, N - 1):
                 if thresholdedImage[i, j] == weak:
